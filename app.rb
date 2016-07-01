6e1b4ffce0bcffe8624188045ad62957a7590709
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-01 20:34:51 +0200

    Add basic machine class

--- app.rb
+++ app.rb
@@ -68,3 +68,18 @@ class Multiply < Struct.new(:left, :right)
 end
 
 
+class Machine < Struct.new(:expression)
+  def step
+    self.expression = expression.reduce
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+
+    puts expression
+  end
+
+end
--- tc_tests.rb
+++ tc_tests.rb
@@ -2,6 +2,7 @@
 
 require_relative "app.rb"
 require "test/unit" 
+require "stringio"
 
 
 class TestReduce < Test::Unit::TestCase
@@ -35,4 +36,30 @@ class TestReduce < Test::Unit::TestCase
     assert(!expression.reducible?)
 
   end
+end
+
+
+
+
+class TestMachine < Test::Unit::TestCase
+
+  def test_run
+    # redirect stdout to string
+    begin
+      old_stdout = $stdout
+      $stdout = StringIO.new('','w')
+
+      Machine.new(
+        Add.new(
+          Multiply.new(Number.new(1), Number.new(2)),
+          Multiply.new(Number.new(3), Number.new(4))
+        )
+      ).run
+
+      assert_equal("1 * 2 + 3 * 4\n2 + 3 * 4\n2 + 12\n14\n", $stdout.string)
+    ensure
+      $stdout = old_stdout
+    end
+  end
+
 end
