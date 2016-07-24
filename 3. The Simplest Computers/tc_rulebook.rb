89a9a42370662527121d72f80a82ff78bbae1746
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-24 21:44:10 +0200

    Add some tests for rulebook

---
+++ 3. The Simplest Computers/tc_rulebook.rb
@@ -0,0 +1,15 @@
+require "test/unit" 
+require_relative 'rulebook.rb'
+
+class TestRulebook < Test::Unit::TestCase
+  def test_rulebook
+    rulebook = DFARulebook.new([
+                 FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
+                 FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
+                 FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)])
+
+   assert_equal(2, rulebook.next_state(1, 'a')) 
+   assert_equal(1, rulebook.next_state(1, 'b'))
+   assert_equal(3, rulebook.next_state(2, 'b'))
+ end
+end
