71ab9c83f0d8a5bd2e750c26b82fc5ef4d0855f1
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-07-25 10:53:38 +0200

    Implement DFA (deterministic finite automaton) class

--- 3. The Simplest Computers/tc_rulebook.rb
+++ 3. The Simplest Computers/tc_rulebook.rb
@@ -2,14 +2,21 @@ require "test/unit"
 require_relative 'rulebook.rb'
 
 class TestRulebook < Test::Unit::TestCase
-  def test_rulebook
-    rulebook = DFARulebook.new([
+  def setup
+  	 @rulebook = DFARulebook.new([
                  FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
                  FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
                  FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)])
-
-   assert_equal(2, rulebook.next_state(1, 'a')) 
-   assert_equal(1, rulebook.next_state(1, 'b'))
-   assert_equal(3, rulebook.next_state(2, 'b'))
+  end
+  
+  def test_rulebook
+   assert_equal(2, @rulebook.next_state(1, 'a')) 
+   assert_equal(1, @rulebook.next_state(1, 'b'))
+   assert_equal(3, @rulebook.next_state(2, 'b'))
  end
+
+  def test_DFA
+  	assert(DFA.new(1, [1,3], @rulebook).accepting?)
+  	assert(!DFA.new(1, [3], @rulebook).accepting?)
+  end
 end
