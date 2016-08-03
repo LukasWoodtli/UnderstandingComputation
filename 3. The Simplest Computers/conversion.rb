545dadf753ab6af191c0e2c1fc93dc4238337f95
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-03 16:07:37 +0200

    Work on conversion from NFA to DFA

--- 3. The Simplest Computers/conversion.rb
+++ 3. The Simplest Computers/conversion.rb
@@ -4,5 +4,9 @@ class NFASimulation < Struct.new(:nfa_design)
   def next_state(state, character)
     nfa_design.to_nfa(state).tap { |nfa| nfa.read_character(character)}.current_states
   end
+  
+  def rules_for(state)
+    nfa_design.rulebook.alphabet.map { |charcter| FARule.new(state, character, next_state(state, character))}
+  end
 end
 
--- 3. The Simplest Computers/tc_conversion.rb
+++ 3. The Simplest Computers/tc_conversion.rb
@@ -6,11 +6,11 @@ require_relative 'conversion.rb'
 class TestConversion < Test::Unit::TestCase
 
   def setup
-    rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'a', 2),
+    @rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'a', 2),
                                 FARule.new(1, nil, 2), FARule.new(2, 'b', 3),
                                 FARule.new(3, 'b', 1), FARule.new(3, nil, 2)])
 
-    @nfa_design = NFADesign.new(1, [3], rulebook)
+    @nfa_design = NFADesign.new(1, [3], @rulebook)
   end
   
   def test_to_nfa
@@ -25,9 +25,7 @@ class TestConversion < Test::Unit::TestCase
     assert_equal(Set[1, 2, 3], nfa.current_states)
 
   end
-end
 
-class TestConversion < Test::Unit::TestCase
   def test_simulation
     simulation = NFASimulation.new(@nfa_design)
     
@@ -37,4 +35,8 @@ class TestConversion < Test::Unit::TestCase
     assert_equal(Set[1, 2, 3], simulation.next_state(Set[1, 2, 3], 'b'))
     assert_equal(Set[1, 2], simulation.next_state(Set[1, 2, 3], 'a'))
   end
+  
+  def test_alphabet
+    assert_equal(["a", "b"], @rulebook.alphabet)
+  end 
 end
