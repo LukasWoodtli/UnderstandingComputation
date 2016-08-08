b90f0b183c34f603410495ebaa1cd0cab413fb98
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-08 22:34:20 +0200

    First implementation for recoginser for palindromes

--- 4. Just Add Power/tc_pda.rb
+++ 4. Just Add Power/tc_pda.rb
@@ -122,3 +122,36 @@ class TestEqualNumberCharacters < Test::Unit::TestCase
   end
 
 end
+
+
+class TestPalindromes < Test::Unit::TestCase
+  def setup
+    @rulebook = DPDARulebook.new([PDARule.new(1, 'a', 1, '$', ['a', '$']),
+                                  PDARule.new(1, 'a', 1, 'a', ['a', 'a']),
+                                  PDARule.new(1, 'a', 1, 'b', ['a', 'b']),
+                                  PDARule.new(1, 'b', 1, '$', ['b', '$']),
+                                  PDARule.new(1, 'b', 1, 'a', ['b', 'a']),
+                                  PDARule.new(1, 'b', 1, 'b', ['b', 'b']),
+                                  PDARule.new(1, 'm', 2, '$', ['$']),
+                                  PDARule.new(1, 'm', 2, 'a', ['a']),
+                                  PDARule.new(1, 'm', 2, 'b', ['b']),
+                                  PDARule.new(2, 'a', 2, 'a', []),
+                                  PDARule.new(2, 'b', 2, 'b', []),
+                                  PDARule.new(2, nil, 3, '$', ['$'])])
+  end
+
+  def test_palindrome
+    dpda_design = DPDADesign.new(1, '$', [3], @rulebook)
+
+    assert(dpda_design.accepts?('abmba'))
+
+    assert(dpda_design.accepts?('babbamabbab'))
+
+    assert(!dpda_design.accepts?('abmb'))
+
+    assert(!dpda_design.accepts?('baambaa'))
+
+  end
+                                  
+end
+
