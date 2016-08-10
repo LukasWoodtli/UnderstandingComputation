68e3d874f56c8bde0ea1b8383938db79b241c3bd
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-10 23:15:27 +0200

    Implementation of NPDA
    Non-deterministic Push-Down Automata

---
+++ 4. Just Add Power/npda.rb
@@ -0,0 +1,49 @@
+require_relative 'dpda.rb'
+
+require 'set'
+
+class NPDARulebook < Struct.new(:rules)
+  def next_configurations(configurations, character)
+    configurations.flat_map { |config|
+      follow_rules_for(config, character) }.to_set
+  end
+
+  def follow_rules_for(configuration, character)
+    rules_for(configuration, character).map { |rule| rule.follow(configuration) }
+  end
+
+  def rules_for(configuration, character)
+    rules.select { |rule| rule.applies_to?(configuration, character) }
+  end
+
+  def follow_free_moves(configurations)
+    more_configurations = next_configurations(configurations, nil)
+
+    if more_configurations.subset?(configurations)
+      configurations
+    else
+      follow_free_moves(configurations + more_configurations)
+    end
+  end
+end
+
+class NPDA < Struct.new(:current_configurations, :accept_states, :rulebook)
+  def accepting?
+    current_configurations.any? { |config| accept_states.include?(config.state) }
+  end
+
+  def read_character(character)
+    self.current_configurations =
+      rulebook.next_configurations(current_configurations, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+
+  def current_configurations
+    rulebook.follow_free_moves(super)
+  end
+end
