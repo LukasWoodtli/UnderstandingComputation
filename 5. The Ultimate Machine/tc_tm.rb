2ed3eeb591fcb2bdc7e44752cb2c723da14a06fd
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-21 10:13:48 +0200

    Improve Turing Machine simulation

--- 5. The Ultimate Machine/tc_tm.rb
+++ 5. The Ultimate Machine/tc_tm.rb
@@ -42,4 +42,58 @@ class TestRule < Test::Unit::TestCase
 
 end
 
+class TestTm < Test::Unit::TestCase
+  def setup
+    @rulebook = DTMRulebook.new([TMRule.new(1, '0', 2, '1', :right),
+                                 TMRule.new(1, '1', 1, '0', :left),
+                                 TMRule.new(1, '_', 2, '1', :right),
+                                 TMRule.new(2, '0', 2, '0', :right),
+                                 TMRule.new(2, '1', 2, '1', :right),
+                                 TMRule.new(2, '_', 3, '_', :left),
+                                ])
+     @tape = Tape.new(['1', '0', '1'], '1', [], '_')
+  end
 
+  def test_configuration
+
+     configuration = TMConfiguration.new(1, @tape)
+    
+     configuration = @rulebook.next_configuration(configuration)
+     assert_equal(TMConfiguration.new(1, Tape.new(['1', '0'], '1', ['0'], '_')), configuration)
+
+     configuration = @rulebook.next_configuration(configuration)
+     assert_equal(TMConfiguration.new(1, Tape.new(['1'], '0', ['0', '0'], '_')), configuration)
+
+     configuration = @rulebook.next_configuration(configuration)
+     assert_equal(TMConfiguration.new(2, Tape.new(['1', '1'], '0', ['0'], '_')), configuration)
+
+  end
+
+  def test_dtm
+    dtm = DTM.new(TMConfiguration.new(1, @tape), [3], @rulebook)
+
+    assert(!dtm.accepting?)
+
+    dtm.step
+    assert_equal(TMConfiguration.new(1, Tape.new(['1', '0'], '1', ['0'], '_')), dtm.current_configuration)
+    assert(!dtm.accepting?)
+
+    dtm.run
+    assert_equal(TMConfiguration.new(3, Tape.new(['1', '1', '0'], '0', ['_'], '_')), dtm.current_configuration)
+    assert(dtm.accepting?)
+  end
+
+  def test_stuck
+    tape = Tape.new(['1', '2', '1'], '1', [], '_')
+    dtm = DTM.new(TMConfiguration.new(1, tape), [3], @rulebook)
+
+    dtm.run
+
+    assert_equal(TMConfiguration.new(1, Tape.new(['1'], '2', ['0', '0'], '_')), dtm.current_configuration)
+    
+    assert(!dtm.accepting?)
+
+    assert(dtm.stuck?)
+  end
+
+end
