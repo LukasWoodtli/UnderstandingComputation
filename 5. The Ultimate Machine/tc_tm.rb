5a35e66c318184bfa8f9aecb6b4496820401c369
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-21 14:30:47 +0200

    Add Turing Machine that recognizes strings

--- 5. The Ultimate Machine/tc_tm.rb
+++ 5. The Ultimate Machine/tc_tm.rb
@@ -9,7 +9,7 @@ class TestTm < Test::Unit::TestCase
     assert_equal('1', tape.middle)
     assert_equal("#<Tape 101(1)>", tape.inspect)
 
-    
+
     assert_equal("#<Tape 10(1)1>", tape.move_head_left.inspect)
 
     assert_equal("#<Tape 101(0)>", tape.write(0).inspect)
@@ -21,7 +21,7 @@ end
 
 
 class TestRule < Test::Unit::TestCase
-  
+
   def setup
     @rule = TMRule.new(1, '0', 2, '1', :right)
   end
@@ -57,7 +57,7 @@ class TestTm < Test::Unit::TestCase
   def test_configuration
 
      configuration = TMConfiguration.new(1, @tape)
-    
+
      configuration = @rulebook.next_configuration(configuration)
      assert_equal(TMConfiguration.new(1, Tape.new(['1', '0'], '1', ['0'], '_')), configuration)
 
@@ -90,10 +90,55 @@ class TestTm < Test::Unit::TestCase
     dtm.run
 
     assert_equal(TMConfiguration.new(1, Tape.new(['1'], '2', ['0', '0'], '_')), dtm.current_configuration)
-    
+
     assert(!dtm.accepting?)
 
     assert(dtm.stuck?)
   end
+end
 
+class StringRecoginzingTM < Test::Unit::TestCase
+  def test_tm
+    rulebook = DTMRulebook.new([
+                    # state 1: scan right looking for 'a'
+                    TMRule.new(1, 'X', 1, 'X', :right), # skip 'X'
+                    TMRule.new(1, 'a', 2, 'X', :right), # cross out 'a' go to state 2
+                    TMRule.new(1, '_', 6, '_', :left), # find blank, go to state 6 (accept)
+
+                    # state 2: scan right looking for 'b'
+                    TMRule.new(2, 'a', 2, 'a', :right), # skip 'a'
+                    TMRule.new(2, 'X', 2, 'X', :right), # skip 'X'
+                    TMRule.new(2, 'b', 3, 'X', :right), # cross out b, go to state 3
+                    
+                    # state 3: scan right looking for 'c'
+                    TMRule.new(3, 'b', 3, 'b', :right), # skip 'b'
+                    TMRule.new(3, 'X', 3, 'X', :right), # skip 'X'
+                    TMRule.new(3, 'c', 4, 'X', :right), # cross out 'c', go to state 4
+
+                    # state 4: scan right looking for end of string
+                    TMRule.new(4, 'c', 4, 'c', :right), # skip 'c'
+                    TMRule.new(4, '_', 5, '_', :left), # find blank, go to state 5
+
+                    # state 5: scan left looking for beginning of string
+                    TMRule.new(5, 'a', 5, 'a', :left), # skip 'a'
+                    TMRule.new(5, 'b', 5, 'b', :left), # skip 'b'
+                    TMRule.new(5, 'c', 5, 'c', :left), # skip 'c'
+                    TMRule.new(5, 'X', 5, 'X', :left), # skip 'X'
+                    TMRule.new(5, '_', 1, '_', :right) # find blank, go to state 1
+                    ])
+
+    tape = Tape.new([], 'a', ['a', 'a', 'b', 'b', 'b', 'c', 'c', 'c'], '_')
+    dtm = DTM.new(TMConfiguration.new(1, tape), [6], rulebook)
+
+    10.times {dtm.step}
+    assert_equal(TMConfiguration.new(5, Tape.new(['X', 'a', 'a', 'X', 'b', 'b', 'X', 'c'], 'c', ['_'], '_')), dtm.current_configuration)
+
+    25.times {dtm.step}
+    assert_equal(TMConfiguration.new(5, Tape.new(['_', 'X', 'X', 'a'], 'X', ['X', 'b', 'X', 'X', 'c', '_'], '_')), dtm.current_configuration)
+
+    dtm.run
+    assert_equal(TMConfiguration.new(6, Tape.new(['_', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 'X', ['_'], '_')), dtm.current_configuration)
+
+
+  end
 end
