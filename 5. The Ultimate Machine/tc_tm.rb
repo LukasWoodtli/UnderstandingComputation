b5422879f4823cb1051cce033d3553e03a4fc08d
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-21 19:09:29 +0200

    Example that TM with internal storage is not needed

--- 5. The Ultimate Machine/tc_tm.rb
+++ 5. The Ultimate Machine/tc_tm.rb
@@ -109,7 +109,7 @@ class StringRecoginzingTM < Test::Unit::TestCase
                     TMRule.new(2, 'a', 2, 'a', :right), # skip 'a'
                     TMRule.new(2, 'X', 2, 'X', :right), # skip 'X'
                     TMRule.new(2, 'b', 3, 'X', :right), # cross out b, go to state 3
-                    
+
                     # state 3: scan right looking for 'c'
                     TMRule.new(3, 'b', 3, 'b', :right), # skip 'b'
                     TMRule.new(3, 'X', 3, 'X', :right), # skip 'X'
@@ -139,6 +139,43 @@ class StringRecoginzingTM < Test::Unit::TestCase
     dtm.run
     assert_equal(TMConfiguration.new(6, Tape.new(['_', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 'X', ['_'], '_')), dtm.current_configuration)
 
-
   end
 end
+
+class TestAdditionalPower < Test::Unit::TestCase
+  def test_tm_with_internal_storage
+    rulebook = DTMRulebook.new([
+                    # state 1: read the first character from the tape
+                    TMRule.new(1, 'a', 2, 'a', :right), # remember 'a'
+                    TMRule.new(1, 'b', 3, 'b', :right), # remember 'b'
+                    TMRule.new(1, 'c', 4, 'b', :right), # remember 'c
+
+                    # state 2: scan right looking for end of string (remembering 'a')
+                    TMRule.new(2, 'a', 2, 'a', :right), # skip 'a'
+                    TMRule.new(2, 'b', 2, 'b', :right), # skip 'b'
+                    TMRule.new(2, 'c', 2, 'c', :right), # skip 'a'
+                    TMRule.new(2, '_', 5, 'a', :right), # find blank, write 'a'
+
+                    # state 3: scan right looking for end of string (remembering 'b')
+                    TMRule.new(3, 'a', 3, 'a', :right), # skip 'a'
+                    TMRule.new(3, 'b', 3, 'b', :right), # skip 'b'
+                    TMRule.new(3, 'c', 3, 'c', :right), # skip 'a'
+                    TMRule.new(3, '_', 5, 'b', :right), # find blank, write 'b'
+
+                    # state 4: scan right looking for end of string (remembering 'c')
+                    TMRule.new(4, 'a', 4, 'a', :right), # skip 'a'
+                    TMRule.new(4, 'b', 4, 'b', :right), # skip 'b'
+                    TMRule.new(4, 'c', 4, 'c', :right), # skip 'a'
+                    TMRule.new(4, '_', 5, 'c', :right)  # find blank, write 'c'
+                    ])
+
+    tape = Tape.new([], 'b', ['c', 'b', 'c', 'a'], '_')
+
+    dtm = DTM.new(TMConfiguration.new(1, tape), [5], rulebook)
+    dtm.run
+
+    assert_equal(Tape.new(['b', 'c', 'b', 'c', 'a', 'b'], '_', [], '_'), dtm.current_configuration.tape)
+  end
+end
+
+
