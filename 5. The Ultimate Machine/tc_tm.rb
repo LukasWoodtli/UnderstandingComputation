5735a9a0ef60e39036bd61d1a045a46436a69e4e
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-21 21:15:29 +0200

    Add example with subroutines

--- 5. The Ultimate Machine/tc_tm.rb
+++ 5. The Ultimate Machine/tc_tm.rb
@@ -176,6 +176,35 @@ class TestAdditionalPower < Test::Unit::TestCase
 
     assert_equal(Tape.new(['b', 'c', 'b', 'c', 'a', 'b'], '_', [], '_'), dtm.current_configuration.tape)
   end
+
+  def test_subroutines
+    added_zero, added_one, added_two, added_three = 0, 1, 2, 3
+    rulebook = DTMRulebook.new(increment_rules(added_zero, added_one) +
+                               increment_rules(added_one, added_two) +
+                               increment_rules(added_two, added_three))
+
+    assert_equal(18, rulebook.rules.length)
+
+    tape = Tape.new(['1', '0', '1'], '1', [], '_')
+    dtm = DTM.new(TMConfiguration.new(added_zero, tape), [added_three], rulebook)
+    dtm.run
+    assert_equal(Tape.new(['1', '1', '1'], '0',['_'], '_'), dtm.current_configuration.tape)
+    
+  end
+
+  def increment_rules(start_state, return_state)
+    incrementing = start_state
+    finishing = Object.new
+    finished = return_state
+    
+    [TMRule.new(incrementing, '0', finishing, '1', :right),
+     TMRule.new(incrementing, '1', incrementing, '0', :left),
+     TMRule.new(incrementing, '_', finishing, '1', :right),
+     TMRule.new(finishing, '0', finishing, '0', :right),
+     TMRule.new(finishing, '1', finishing, '1', :right),
+     TMRule.new(finishing, '_', finished, '_', :left)]
+  end
+
 end
 
 
