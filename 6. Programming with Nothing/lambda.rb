9c5e6bac35fc411f930a631d242867b65cfe507b
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-23 19:46:26 +0200

    Implement FizzBuzz with lambda calculus

--- 6. Programming with Nothing/lambda.rb
+++ 6. Programming with Nothing/lambda.rb
@@ -33,6 +33,59 @@ SLIDE = -> p {PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]]}
 # decrement
 DECREMENT = -> n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
 
+# addition, subtraction, multiplication, power
+ADD = -> m { -> n { n[INCREMENT][m] }}
+SUBTRACT = -> m { -> n { n[DECREMENT][m] }}
+
+MULTIPLY = -> m { -> n { n[ADD[m]][ZERO]}}
+
+POWER = -> m { -> n { n[MULTIPLY[m]][ONE]}}
+
+# less or equal
+IS_LESS_OR_EQUAL = -> m { -> n { IS_ZERO[SUBTRACT[m][n]] }}
+
+# Y- and Z-combinator
+Y = -> f { -> x { f[x[x]] } [ -> x { f[x[x]] } ] } # loops forever in ruby: eager evaluation
+Z = -> f { -> x { f[-> y {x[x][y]}] } [ -> x { f[ -> y {x[x][y]}] } ] }
+
+# modulo
+MOD = Z[ -> f { -> m { -> n { IF[IS_LESS_OR_EQUAL[n][m]][-> x {f[SUBTRACT[m][n]][n][x]}][m] }}}]
+
+# lists
+EMPTY = PAIR[TRUE][TRUE]
+UNSHIFT = -> l { -> x { PAIR[FALSE][PAIR[x][l]]}}
+IS_EMPTY = LEFT
+FIRST = -> l {LEFT[RIGHT[l]]}
+REST = -> l {RIGHT[RIGHT[l]]}
+
+# range
+RANGE = Z[-> f { -> m { -> n { IF[IS_LESS_OR_EQUAL[m][n]][ -> x { UNSHIFT[f[INCREMENT[m]][n]][m][x]}][EMPTY]}}}]
+
+# fold, map
+FOLD = Z[-> f { -> l { -> x { -> g {IF[IS_EMPTY[l]][x][-> y { g[f[REST[l]][x][g]][FIRST[l]][y]} ]}}}}]
+MAP = -> k { -> f { FOLD[k][EMPTY][ -> l { -> x {UNSHIFT[l][f[x]]}}]}}
+
+# push to list
+PUSH = -> l { -> x { FOLD[l][UNSHIFT[EMPTY][x]][UNSHIFT] }}
+
+# division
+DIV = Z[-> f { -> m { -> n { IF[IS_LESS_OR_EQUAL[n][m]][ -> x { INCREMENT[f[SUBTRACT[m][n]][n]][x]}][ZERO] }}}]
+
+# more numbers and chars
+TEN = MULTIPLY[TWO][FIVE]
+B = TEN
+F = INCREMENT[B]
+I = INCREMENT[F]
+U = INCREMENT[I]
+ZED = INCREMENT[U]
+
+# integer to list of digits
+TO_DIGITS = Z[ -> f { -> n { PUSH[IF[IS_LESS_OR_EQUAL[n][DECREMENT[TEN]]][EMPTY][ -> x {f[DIV[n][TEN]][x]} ]][MOD[n][TEN]]}}]
+
+FIZZ = UNSHIFT[UNSHIFT[UNSHIFT[UNSHIFT[EMPTY][ZED]][ZED]][I]][F]
+BUZZ = UNSHIFT[UNSHIFT[UNSHIFT[UNSHIFT[EMPTY][ZED]][ZED]][U]][B]
+FIZZBUZZ = UNSHIFT[UNSHIFT[UNSHIFT[UNSHIFT[BUZZ][ZED]][ZED]][I]][F]
+
 def to_integer(proc)
   proc[->n {n + 1}][0]
 end
@@ -40,3 +93,24 @@ end
 def to_boolean(proc)
   IF[proc][true][false]
 end
+
+def to_array(proc)
+  array = []
+
+  until to_boolean(IS_EMPTY[proc])
+    array.push(FIRST[proc])
+    proc = REST[proc]
+  end
+
+  array
+end
+
+def to_char(c)
+  '0123456789BFiuz'.slice(to_integer(c))
+end
+
+def to_string(s)
+  to_array(s).map { |c| to_char(c) }.join
+end
+
+
--- 6. Programming with Nothing/tc_lambda.rb
+++ 6. Programming with Nothing/tc_lambda.rb
@@ -50,5 +50,88 @@ class TestLambda < Test::Unit::TestCase
     assert_equal(99, to_integer(DECREMENT[HUNDRED]))
   end
 
+  def test_numerical_operations
+    assert_equal(4, to_integer(ADD[ONE][THREE]))
+    assert_equal(13, to_integer(SUBTRACT[FIFTEEN][TWO]))
+    assert_equal(15, to_integer(MULTIPLY[THREE][FIVE]))
+    assert_equal(8, to_integer(POWER[TWO][THREE]))
+  end
 
+  def test_is_less_or_equal
+    assert(to_boolean(IS_LESS_OR_EQUAL[ONE][TWO]))
+    assert(to_boolean(IS_LESS_OR_EQUAL[TWO][TWO]))
+    assert(!to_boolean(IS_LESS_OR_EQUAL[THREE][TWO]))
+  end
+
+  def test_mod
+    assert_equal(1, to_integer(MOD[THREE][TWO]))
+    assert_equal(2, to_integer(MOD[POWER[THREE][THREE]][ADD[THREE][TWO]]))
+  end
+
+  def test_list
+    my_list = UNSHIFT[UNSHIFT[UNSHIFT[EMPTY][THREE]][TWO]][ONE]
+
+    assert_equal(1, to_integer(FIRST[my_list]))
+    assert_equal(2, to_integer(FIRST[REST[my_list]]))
+    assert_equal(3, to_integer(FIRST[REST[REST[my_list]]]))
+
+    assert(!to_boolean(IS_EMPTY[my_list]))
+    assert(to_boolean(IS_EMPTY[EMPTY]))
+  end
+
+  def test_range
+    my_range = RANGE[ONE][FIVE]
+    assert_equal([1,2,3,4,5], to_array(my_range).map{ |p| to_integer(p)})
+  end
+
+  def test_fold
+    assert_equal(15, to_integer(FOLD[RANGE[ONE][FIVE]][ZERO][ADD]))
+    assert_equal(120, to_integer(FOLD[RANGE[ONE][FIVE]][ONE][MULTIPLY]))
+  end
+
+  def test_map
+    my_list = MAP[RANGE[ONE][FIVE]][INCREMENT]
+    assert_equal([2, 3, 4, 5, 6], to_array(my_list).map { |p| to_integer(p)})
+  end
+
+  def test_strings
+    assert_equal('z', to_char(ZED))
+    assert_equal('FizzBuzz', to_string(FIZZBUZZ))
+    
+    assert_equal("5", to_string(TO_DIGITS[FIVE]))
+    assert_equal("125", to_string(TO_DIGITS[POWER[FIVE][THREE]]))
+  end
+
+  def test_to_digits
+    assert_equal([5], to_array(TO_DIGITS[FIVE]).map { |p| to_integer(p)})
+    assert_equal([1, 2, 5], to_array(TO_DIGITS[POWER[FIVE][THREE]]).map { |p| to_integer(p)})
+  end
+
+  def test_fizz_buzz
+    solution = MAP[RANGE[ONE][HUNDRED]][ -> n {
+                 IF[IS_ZERO[MOD[n][FIFTEEN]]][
+                   FIZZBUZZ
+                 ][IF[IS_ZERO[MOD[n][THREE]]][
+                   FIZZ
+                 ][IF[IS_ZERO[MOD[n][FIVE]]][
+                   BUZZ
+                 ][
+                   TO_DIGITS[n]
+                 ]]]
+                 }]
+    assert_equal(ruby_fizz_buzz, to_array(solution).map {|p| to_string(p)})
+  end
+
+  def ruby_fizz_buzz
+    (1..100).map { |n| 
+      if(n % 15).zero?
+        'FizzBuzz'
+      elsif(n % 3).zero?
+        'Fizz'
+      elsif(n % 5).zero?
+        'Buzz'
+      else
+        n.to_s
+      end }
+  end
 end
