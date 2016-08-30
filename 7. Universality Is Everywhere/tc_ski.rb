3cc16547110385c2c379dd9d0072d4c31f0dbbdd
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-30 21:16:57 +0200

    Add some more tests

--- 7. Universality Is Everywhere/tc_ski.rb
+++ 7. Universality Is Everywhere/tc_ski.rb
@@ -12,5 +12,23 @@ class TestSKI < Test::Unit::TestCase
 
     y, z = SKISymbol.new(:y), SKISymbol.new(:z)
     assert_equal("x[z][y[z]]", S.call(x, y, z).to_s)
+
+    expression = SKICall.new(SKICall.new(SKICall.new(S, x), y),z)
+    assert_equal("S[x][y][z]", expression.to_s)
+
+    combinator = expression.left.left.left
+    assert_equal("S", combinator.to_s)
+
+    first_argument = expression.left.left.right
+    assert_equal("x", first_argument.to_s)
+
+    second_argument = expression.left.right
+    assert_equal("y", second_argument.to_s)
+
+    third_argument = expression.right
+    assert_equal("z", third_argument.to_s)
+
+    test = combinator.call(first_argument, second_argument, third_argument)
+    assert_equal("x[z][y[z]]", test.to_s)
   end
 end
