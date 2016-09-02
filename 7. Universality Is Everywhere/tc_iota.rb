89767f96744d05e8159b6572936dee5f43b592aa
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-09-02 19:56:18 +0200

    Add example to convert lambda calculus to iota

--- 7. Universality Is Everywhere/tc_iota.rb
+++ 7. Universality Is Everywhere/tc_iota.rb
@@ -3,6 +3,8 @@ require "test/unit"
 
 require_relative 'iota.rb'
 
+require 'treetop'
+
 class TestIota < Test::Unit::TestCase
   def test_reduce_s
     expression = S.to_iota
@@ -48,8 +50,29 @@ class TestIota < Test::Unit::TestCase
       expression = expression.reduce
     end
 
-    assert_equal("x", expression.to_s)    
-    
+    assert_equal("x", expression.to_s)
+  end
+
+  def test_lambda_to_iota
+    this_dir = File.expand_path(File.dirname(__FILE__))
+    Treetop.load(File.join(this_dir, '../6. Programming with Nothing/lambda_calculus'))
+
+    two = LambdaCalculusParser.new.parse("-> p { -> x { p[p[x]] } }").to_ast
+  
+    assert_equal("S[S[K[S]][S[K[K]][I]]][S[S[K[S]][S[K[K]][I]]][K[I]]]",
+                 two.to_ski.to_s)
+
+    assert_equal("ɩ[ɩ[ɩ[ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ]]][ɩ[ɩ[ɩ[ɩ]]]]][ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]]]][ɩ[ɩ[ɩ[ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ]]][ɩ[ɩ[ɩ[ɩ]]]]][ɩ[ɩ]]]][ɩ[ɩ[ɩ[ɩ]]][ɩ[ɩ]]]]", two.to_ski.to_iota.to_s)
+
+
+    inc = SKISymbol.new(:inc)
+    zero = SKISymbol.new(:zero)
+
+    expression = SKICall.new(SKICall.new(two.to_ski.to_iota, inc), zero)
+
+    expression = expression.reduce while expression.reducible?
+
+    assert_equal("inc[inc[zero]]", expression.to_s)
 
   end
 end
