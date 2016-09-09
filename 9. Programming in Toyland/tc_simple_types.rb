7117facdb07fb8173a6ae866411a8ef8777086c9
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-09-09 22:15:34 +0200

    Add more examples (tests)

--- 9. Programming in Toyland/tc_simple_types.rb
+++ 9. Programming in Toyland/tc_simple_types.rb
@@ -53,5 +53,57 @@ class TestTypes < Test::Unit::TestCase
                                                               Number.new(1)))))
     assert_equal("x = 0; while (true) { x = x + 1 }", statement.to_s)
     assert_equal(Type::VOID, statement.type({ x: Type::NUMBER }))
+
+    # This would loop forever
+    # statement.evaluate({})
+
+    statement = Sequence.new(statement,
+                             Assign.new(:x, Boolean.new(true)))
+
+    assert_equal(nil, statement.type({ x: Type::NUMBER }))
+  end
+
+  def test_restrictive
+    statement = Sequence.new(
+      If.new(Variable.new(:b),
+             Assign.new(:x, Number.new(6)),
+             Assign.new(:x, Boolean.new(true))
+             ),
+      Sequence.new(If.new(Variable.new(:b),
+                          Assign.new(:y, Variable.new(:x)),
+                          Assign.new(:y, Number.new(1))
+                          ),
+                   Assign.new(:z, Add.new(Variable.new(:y), Number.new(1)))))
+
+    assert_equal(
+      "if (b) { x = 6 } else { x = true }; if (b) { y = x } else { y = 1 }; z = y + 1", 
+      statement.to_s)
+  
+    assert_equal(
+      { b: Boolean.new(true), x: Number.new(6), y: Number.new(6), z: Number.new(7)},
+      statement.evaluate({ b: Boolean.new(true) }))
+
+    assert_equal(
+      { b: Boolean.new(false), x: Boolean.new(true), y: Number.new(1), z: Number.new(2)},
+      statement.evaluate({ b: Boolean.new(false) }))
+
+
+    assert_equal(nil, statement.type({}))
+
+    context = { b: Type::BOOLEAN, y: Type::NUMBER, z: Type::NUMBER }
+    assert_equal(nil, statement.type(context))
+    assert_equal(nil, statement.type(context.merge({ x: Type::NUMBER })))
+    assert_equal(nil, statement.type(context.merge({ x: Type::BOOLEAN })))
+
+  end
+
+  def test_unasign
+    statement = Assign.new(:x, Add.new(Variable.new(:x),
+                                       Number.new(1)))
+
+    assert_equal(Type::VOID, statement.type({ x: Type::NUMBER }))
+
+    # this fails even if typing check succeded
+    # statement.evaluate({})
   end
 end
